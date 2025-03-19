d5d2c668b0a7d50b840f8a436b71d8b27a43b15e
Anuj Sapkota <anujsapkota808@gmail.com>
2025-03-19 22:32:46 +0545

    Data_types

---
+++ data_types/data_types.py
@@ -0,0 +1,33 @@
+# first = "Anuj"
+# last = "Sapkota"
+
+# #Print
+# print(type(first))
+# print(type(first) == str)
+# # Strings are instance of Str class
+
+# # Casting number into string
+# decade = str(1980)
+# print(type(decade))
+# print(decade)
+
+# Multiple Lines
+
+multilineString = """This 
+is
+a 
+multiline
+String."""
+print(multilineString)
+
+# escape special characters
+    # Print a single quote
+print("I\'m a programmer.\tI am learning Python.\nI am enjoying\\it.")
+
+#String Methods
+# .lower() - converts string to lower case
+# .upper() - converts string to upper case
+# .title() - converts string to title case
+#print("anuj sapkota".title())
+print("Anuj".replace("N","B"))
+
